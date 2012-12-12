21093d8de02eac08f994a1573179db6679cc0893
Michał Smulski <smulski.michal@gmail.com>
2012-12-12 22:59:40 +0100

    Initial Commit

---
+++ CollectionView/TPViewController.h
@@ -0,0 +1,13 @@
+//
+//  TPViewController.h
+//  CollectionView
+//
+//  Created by Michał Smulski on 12.12.2012.
+//  Copyright (c) 2012 tap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TPViewController : UIViewController
+
+@end
---
+++ CollectionView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CollectionView
+//
+//  Created by Michał Smulski on 12.12.2012.
+//  Copyright (c) 2012 tap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([TPAppDelegate class]));
+	}
+}
